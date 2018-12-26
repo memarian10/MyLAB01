10aa5d0e9467379cebfde8daebe28a7f965f361e
Nima Memarian <memarian10@hotmail.com>
2018-12-26 17:25:53 +0330

    Add project files.

---
+++ MyLAB01/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MyLAB01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtStr1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtStr2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.txtStr3 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblOut1 = new System.Windows.Forms.Label();
+            this.lblOut2 = new System.Windows.Forms.Label();
+            this.lblOut3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "String1";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(68, 23);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 0;
+            this.txtNum1.Text = "0";
+            // 
+            // txtStr1
+            // 
+            this.txtStr1.Location = new System.Drawing.Point(68, 54);
+            this.txtStr1.Name = "txtStr1";
+            this.txtStr1.Size = new System.Drawing.Size(100, 20);
+            this.txtStr1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Number2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "String2";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(296, 23);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 1;
+            this.txtNum2.Text = "0";
+            // 
+            // txtStr2
+            // 
+            this.txtStr2.Location = new System.Drawing.Point(296, 54);
+            this.txtStr2.Name = "txtStr2";
+            this.txtStr2.Size = new System.Drawing.Size(100, 20);
+            this.txtStr2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Number3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(461, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "String3";
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Location = new System.Drawing.Point(517, 23);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(100, 20);
+            this.txtNum3.TabIndex = 2;
+            this.txtNum3.Text = "0";
+            // 
+            // txtStr3
+            // 
+            this.txtStr3.Location = new System.Drawing.Point(517, 54);
+            this.txtStr3.Name = "txtStr3";
+            this.txtStr3.Size = new System.Drawing.Size(100, 20);
+            this.txtStr3.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(68, 111);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblOut1
+            // 
+            this.lblOut1.AutoSize = true;
+            this.lblOut1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut1.Location = new System.Drawing.Point(308, 174);
+            this.lblOut1.Name = "lblOut1";
+            this.lblOut1.Size = new System.Drawing.Size(88, 25);
+            this.lblOut1.TabIndex = 4;
+            this.lblOut1.Text = "Output1";
+            // 
+            // lblOut2
+            // 
+            this.lblOut2.AutoSize = true;
+            this.lblOut2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut2.Location = new System.Drawing.Point(308, 243);
+            this.lblOut2.Name = "lblOut2";
+            this.lblOut2.Size = new System.Drawing.Size(88, 25);
+            this.lblOut2.TabIndex = 4;
+            this.lblOut2.Text = "Output2";
+            // 
+            // lblOut3
+            // 
+            this.lblOut3.AutoSize = true;
+            this.lblOut3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut3.Location = new System.Drawing.Point(308, 314);
+            this.lblOut3.Name = "lblOut3";
+            this.lblOut3.Size = new System.Drawing.Size(88, 25);
+            this.lblOut3.TabIndex = 4;
+            this.lblOut3.Text = "Output3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 378);
+            this.Controls.Add(this.lblOut3);
+            this.Controls.Add(this.lblOut2);
+            this.Controls.Add(this.lblOut1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtStr3);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtStr2);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtStr1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MyLab";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtStr1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtStr2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.TextBox txtStr3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblOut1;
+        private System.Windows.Forms.Label lblOut2;
+        private System.Windows.Forms.Label lblOut3;
+    }
+}
+
---
+++ MyLAB01/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLAB01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            string str1 = "";
+            string str2 = "" ;
+            string str3 = "";
+
+            num1 = Convert.ToInt32(txtNum1.Text);
+            num2 = Convert.ToInt32(txtNum2.Text);
+            num3 = Convert.ToInt32(txtNum3.Text);
+            str1 = txtStr1.Text;
+            str2 = txtStr2.Text;
+            str3 = txtStr3.Text;
+
+            /////////////////////////////////////////////////////////
+            //IF_ELSE
+
+            //if (num1 < 10) lblOut1.Text = "Less than 10"; else lblOut1.Text = "Grater than 10";
+
+            //if (num1 < 10)
+            //{
+            //    lblOut2.Text = "Less than 10";
+            //}
+            //else if(num1>10)
+            //{
+            //    lblOut2.Text = "Grater than 10";
+            //}
+            //else
+            //{
+            //    lblOut2.Text = "Equal 10";
+            //}
+
+            //
+        }
+    }
+}
