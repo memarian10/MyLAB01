98a8f52ad427f6b2252ba51dff0451e55148b3fe
Nima Memarian <memarian10@hotmail.com>
2019-01-01 12:20:18 +0330

    2D Array

--- MyLAB01/Form1.cs
+++ MyLAB01/Form1.cs
@@ -20,12 +20,14 @@ namespace MyLAB01
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             lblOut1.Text = "";
+            lblOut2.Text = "";
+            lblOut3.Text = "";
             ////This is for GITHUB
             int num1 = 0;
             int num2 = 0;
             int num3 = 0;
             string str1 = "";
-            string str2 = "" ;
+            string str2 = "";
             string str3 = "";
 
             num1 = Convert.ToInt32(txtNum1.Text);
@@ -96,15 +98,69 @@ namespace MyLAB01
             //    lblOut1.Text += "\n";
             //}
 
-            for (int i = 0; i < 10; i++)
+            //Continue
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i==5 || i==9)
+            //    {
+            //        continue;
+            //    }
+            //    lblOut1.Text += ("\nLine" + i);
+            //}
+
+            //array fill by input
+            //int[] arr = new int[3];
+            //arr[0] = num1;
+            //arr[1] = num2;
+            //arr[2] = num3;
+
+            //foreach (var item in arr)
+            //{
+            //    lblOut1.Text += (item.ToString() + "-");
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    lblOut2.Text += (arr[i].ToString() + "-");
+            //}
+
+            //VAR Data Type
+            //var tst1 = "";
+            //var tst2 = 0;
+
+            //tst1 = str1;
+            //tst2 = num1;
+
+            //lblOut1.Text = tst1;
+            //lblOut2.Text = tst2.ToString();
+
+            //2D Array
+            //int[,] arr2d = new int[3, 2] {{ 1, 2 }, { 4, 5 }, { 7, 8} };
+            //foreach (var item in arr2d)
+            //{
+            //    lblOut1.Text += (item.ToString() + "\n");
+            //}
+
+            //Average of 3 students for 4 lesson
+            int[,] arr2d = new int[3, 4] {{ 10, 17, 16, 14 }, { 18, 9, 19, 17}, { 15, 11, 12, 14} };
+            int[] sum = new int[4];
+
+            for (int i = 0; i < arr2d.GetLength(0); i++)
             {
-                if (i==5 || i==9)
+                lblOut1.Text = i.ToString();
+                lblOut3.Text += arr2d[i, 0].ToString()+ "  ";
+                
+                for (int j = 0; j < arr2d.GetLength(1); j++)
                 {
-                    continue;
+                    lblOut2.Text += arr2d[i, j].ToString()+ "  ";
+                    sum[i] += arr2d[i, j];
                 }
-                lblOut1.Text += ("\nLine" + i);
+                lblOut1.Text = sum[i].ToString();
+                lblOut2.Text += "\n";
             }
             
+
+
         }
     }
 }
